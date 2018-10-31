4d26b6ee58ffa41d96af94cc6647f4c1bfe90345
Mehmet Baker <mehmet.baker@gmail.com>
2018-11-01 00:25:10 +0300

    Fixed pinned tab navigation issue
    Tab navigation is re-enabled if the active tab is a pinned tab (even if
    the user has selected to skip pinned tabs). So you can always navigate from a
    pinned tab but if you want to navigate to a pinned tab you should first
    deselect the option to skip pinned tabs.
    
    Signed-off-by: Mehmet Baker <mehmet.baker@gmail.com>

--- background.js
+++ background.js
@@ -43,23 +43,27 @@ async function activateTab(currentTab, commandRepetition, direction) {
 
     switch (direction) {
       case 'next':
-      //If no repetition, then we just want to navigate to the next tab
-      if (commandRepetition == '' || commandRepetition == '0') {
-        tabIndex = (index + 1) % tabCount;
-      } else if (+commandRepetition <= tabCount) {
-        //This is a special case: 5gt means "go to the 5th tab"
-        //@see {@link http://vim.wikia.com/wiki/Using_tab_pages|Using tab pages} Navigation section
-        //Since the tab index is zero-based, subtract 1 from the desired index.
-        tabIndex = +commandRepetition - 1;
-      }
-      break;
+        if (skipPinnedTabs && currentTab.pinned) index = -1;
+
+        //If no repetition, then we just want to navigate to the next tab
+        if (commandRepetition == '' || commandRepetition == '0') {
+          tabIndex = (index + 1) % tabCount;
+        } else if (+commandRepetition <= tabCount) {
+          //This is a special case: 5gt means "go to the 5th tab"
+          //@see {@link http://vim.wikia.com/wiki/Using_tab_pages|Using tab pages} Navigation section
+          //Since the tab index is zero-based, subtract 1 from the desired index.
+          tabIndex = +commandRepetition - 1;
+        }
+        break;
 
       case 'prev':
-      tabIndex = positiveModulo(index - +commandRepetition, tabCount);
-      break;
+        if (skipPinnedTabs && currentTab.pinned) index = 0;
+
+        tabIndex = positiveModulo(index - +commandRepetition, tabCount);
+        break;
 
       default:
-      break;
+        break;
     }
 
     if (tabIndex !== null) {
